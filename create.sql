7b2ec02a21b1abbc5358ea7b60a0a52b5d043cc4
Zaú Júlio <zauhdf@gmail.com>
2021-04-02 02:43:32 -0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS`db_library`;
 CREATE SCHEMA db_library
 	CHARACTER SET 'utf8'
     COLLATE 'utf8_unicode_ci';
@@ -5,80 +6,9 @@ CREATE SCHEMA db_library
 use db_library;
 SET time_zone = 'UTC';
 
-CREATE TABLE author (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE gender (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE publisher (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    cnpj VARCHAR(18) NOT NULL,
-    telephone VARCHAR(16) NOT NULL,
-    email VARCHAR(256) NOT NULL
-);
-
-CREATE TABLE category (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE book (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    cod_author INT NOT NULL,
-    cod_publisher INT NOT NULL,
-    cod_gender INT NOT NULL,
-    cod_category INT NOT NULL,
-    isbn BIGINT NOT NULL,
-    unity BIGINT NOT NULL,
-    title VARCHAR(100) NOT NULL,
-    subtitle VARCHAR(50),
-    status NUMERIC NOT NULL,
-    CONSTRAINT fk_book_category FOREIGN KEY (cod_category)
-        REFERENCES category (id)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE,
-    CONSTRAINT fk_book_gender FOREIGN KEY (cod_gender)
-        REFERENCES gender (id)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE,
-    CONSTRAINT fk_book_publisher FOREIGN KEY (cod_publisher)
-        REFERENCES publisher (id)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE,
-    CONSTRAINT fk_book_author FOREIGN KEY (cod_author)
-        REFERENCES author (id)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE
-);
-
-CREATE TABLE functionary (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE operation (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    description VARCHAR(100) NOT NULL,
-    cod_functionary INT NOT NULL,
-    day NUMERIC NOT NULL,
-    horary_start TIME NOT NULL,
-    horary_end TIME NOT NULL,
-    workload NUMERIC NOT NULL,
-    CONSTRAINT fk_operation_functionary FOREIGN KEY (cod_functionary)
-        REFERENCES functionary (id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-);
 
 CREATE TABLE address (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     street VARCHAR(50) NOT NULL,
     neighborhood VARCHAR(50),
     zipcode NUMERIC NOT NULL,
@@ -91,31 +21,111 @@ CREATE TABLE address (
 CREATE TABLE library (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(50) NOT NULL,
-    cod_operation INT NOT NULL,
-    CONSTRAINT fk_library_operation FOREIGN KEY (id)
-        REFERENCES operation (id)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE
+    cod_address INT NOT NULL,
+    CONSTRAINT fk_library_address FOREIGN KEY (cod_address)
+        REFERENCES address (id)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE author (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE gender (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE category (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE publisher (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    cnpj VARCHAR(18) NOT NULL,
+    telephone VARCHAR(16) NOT NULL,
+    email VARCHAR(256) NOT NULL,
+    cod_address INT NOT NULL,
+    CONSTRAINT fk_publisher_address FOREIGN KEY (cod_address)
+        REFERENCES address (id)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE book (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    isbn BIGINT NOT NULL,
+    unity BIGINT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    subtitle VARCHAR(50),
+    status NUMERIC NOT NULL,
+    cod_author INT NOT NULL,
+    cod_publisher INT NOT NULL,
+    cod_gender INT NOT NULL,
+    cod_category INT NOT NULL,
+    CONSTRAINT fk_book_category FOREIGN KEY (cod_category)
+        REFERENCES category (id)
+        ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_book_gender FOREIGN KEY (cod_gender)
+        REFERENCES gender (id)
+        ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_book_publisher FOREIGN KEY (cod_publisher)
+        REFERENCES publisher (id)
+        ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_book_author FOREIGN KEY (cod_author)
+        REFERENCES author (id)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE functionary (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    cpf BIGINT NOT NULL,
+    telephone VARCHAR(16) NOT NULL,
+    email VARCHAR(256) NOT NULL,
+    salary FLOAT NOT NULL,
+    cod_address INT NOT NULL,
+    CONSTRAINT fk_functionary_address FOREIGN KEY (cod_address)
+        REFERENCES address (id)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE operation (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    day NUMERIC NOT NULL,
+    horary_start TIME NOT NULL,
+    horary_end TIME NOT NULL,
+    workload NUMERIC NOT NULL,
+    cod_functionary INT NOT NULL,
+    cod_library INT NOT NULL,
+    CONSTRAINT fk_operation_functionary FOREIGN KEY (cod_functionary)
+        REFERENCES functionary (id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_operation_library FOREIGN KEY (cod_library)
+        REFERENCES library (id)
+        ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE user (
     id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    cod_address BIGINT NOT NULL,
-    cod_library INT NOT NULL,
-    username VARCHAR(100) NOT NULL,
+    username VARCHAR(20) NOT NULL,
     name VARCHAR(100) NOT NULL,
-    telephone VARCHAR(14) NOT NULL,
+    telephone VARCHAR(16) NOT NULL,
     email VARCHAR(256) NOT NULL,
     password VARCHAR(32) NOT NULL,
-    status NUMERIC NOT NULL,
+    status INT NOT NULL,
+    cod_address INT NOT NULL,
+    cod_library INT NOT NULL,
     CONSTRAINT fk_user_address FOREIGN KEY (cod_address)
         REFERENCES address (id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-  CONSTRAINT fk_user_library FOREIGN KEY (cod_library)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_user_library FOREIGN KEY (cod_library)
         REFERENCES library (id)
-		    ON DELETE RESTRICT
-        ON UPDATE CASCADE
+        ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE loan (
@@ -126,12 +136,10 @@ CREATE TABLE loan (
     cod_user BIGINT NOT NULL,
     CONSTRAINT fk_loan_book FOREIGN KEY (cod_book)
         REFERENCES book (id)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE,
+        ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT fk_loan_user FOREIGN KEY (cod_user)
         REFERENCES user (id)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE
+        ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE session (
@@ -141,14 +149,21 @@ CREATE TABLE session (
     cod_library INT NOT NULL,
     CONSTRAINT fk_session_category FOREIGN KEY (cod_category)
         REFERENCES category (id)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE,
+        ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT fk_session_library FOREIGN KEY (cod_library)
         REFERENCES library (id)
-		    ON DELETE RESTRICT
-        ON UPDATE CASCADE
+        ON DELETE RESTRICT ON UPDATE CASCADE
 );
-             
+
+INSERT INTO address(id, street, neighborhood, zipcode, city, state, country, number)
+  VALUES
+    (NULL, "Rua João Pedro", NULL, 59300000, "Caíco", "RN", "Brasil", 35),
+    (NULL, "Av Pedro Augosto", NULL, 39840923, "João Pessoa", "PB", "Brasil", 64),
+    (NULL, "Rua Roraraima Braga", NULL, 56632236, "Bentinho", "RS", "Brasil", 5643),
+    (NULL, "Av Júlio César", NULL, 62353000, "São Paulo", "SP", "Brasil", 543),
+    (NULL, "Rua João Gomes", NULL, 24323674, "Natal", "RN", "Brasil", 85)
+;
+
 INSERT INTO author(id, name)
   VALUES
     (NULL, "João"),
@@ -167,15 +182,6 @@ INSERT INTO gender(id, name)
     (NULL, "Aventura")
 ;
 
-INSERT INTO publisher(id, name, cnpj, telephone, email)
-  VALUES
-    (NULL, "Iluz", "65.500.846/0001-36","(84)2344-3443", "iluz@edt.com"),
-    (NULL, "PubEdit", "79.985.005/0001-70","(84)6453-4543", "pubEdit@gmail.com"),
-    (NULL, "Kabum", "70.067.312/0001-06","(84)5329-3783", "kabum@hyper.com"),
-    (NULL, "Porto", "17.508.480/0001-07","(84)0993-3443", "porto@uol.com"),
-    (NULL, "Mega", "65.202.121/0001-61","(84)4928-1243", "mega@mega.editor.com")
-;
-
 INSERT INTO category (id, name)
   VALUES
     (NULL, "Matemática"),
@@ -183,33 +189,6 @@ INSERT INTO category (id, name)
     (NULL, "História"),
     (NULL, "Filosofia"),
     (NULL, "Ajuda")
-;
-
-INSERT INTO book(id, cod_author, cod_publisher, isbn, unity, title, subtitle, status, cod_gender, cod_category)
-  VALUES
-    (NULL, 1, 3, 9788541106993, 1, "Aleatório", NULL, 0, 1, 4),
-    (NULL, 2, 2, 7968525816632, 1, "História é tudo que me deixou", NULL, 0, 2, 1),
-    (NULL, 2, 4, 8373571800587, 1, "Vermelho, Branco e Sangue Azul", NULL, 0, 3, 2),
-    (NULL, 3, 1, 5614827216374, 1, "15 Dias", NULL, 0, 4, 5),
-    (NULL, 4, 5, 2125251257992, 1, "Entre o Céu e o Inferno", NULL, 0, 5, 5)
-;
-
-INSERT INTO functionary(id, name)
-  VALUES
-    (NULL, "Gustavo"),
-    (NULL, "Augusto"),
-    (NULL, "Xuxa"),
-    (NULL, "Mário"),
-    (NULL, "Sonic")
-;
-
-INSERT INTO operation(id, cod_functionary, name, description, day, horary_start, horary_end, workload)
-  VALUES
-    (NULL, 1, "Limpeza", "Realiza limpeza dos andares inferiores", 1,"08:00:00", "18:00:00", 50),
-    (NULL, 2, "Atendente", "Atendimento", 2,"08:00:00", "12:00:00", 20),
-    (NULL, 3, "Devolução", "Gestão de devolução", 5,"08:00:00", "13:00:00", 25),
-    (NULL, 4, "Empréstimo", "Gestão de empréstimos", 3,"08:00:00", "18:00:00", 50),
-    (NULL, 5, "Fornecimento", "Recebimento de novos livros", 4,"08:00:00", "17:00:00", 45)
 ;
 
 INSERT INTO author(id, name)
@@ -221,22 +200,49 @@ INSERT INTO author(id, name)
     (NULL, "Emanuel")
 ;
 
-INSERT INTO address(id, street, neighborhood, zipcode, city, state, country, number)
-  VALUES
-    (NULL, "Rua João Pedro", NULL, 59300000, "Caíco", "RN", "Brasil", 35),
-    (NULL, "Av Pedro Augosto", NULL, 39840923, "João Pessoa", "PB", "Brasil", 64),
-    (NULL, "Rua Roraraima Braga", NULL, 56632236, "Bentinho", "RS", "Brasil", 5643),
-    (NULL, "Av Júlio César", NULL, 62353000, "São Paulo", "SP", "Brasil", 543),
-    (NULL, "Rua João Gomes", NULL, 24323674, "Natal", "RN", "Brasil", 85)
-;
-
-INSERT INTO library (id, name, cod_operation)
+INSERT INTO library (id, name, cod_address)
   VALUES
     (NULL, "Biblioteca Maria Texeira", 1),
     (NULL, "Biblioteca Municipal", 2),
     (NULL, "Biblioteca Pedro Alvez", 5),
     (NULL, "Biblioteca João&Maria", 3),
     (NULL, "Biblioteca Carlos Cabral", 4)
+;
+
+INSERT INTO publisher(id, name, cnpj, telephone, email, cod_address)
+  VALUES
+    (NULL, "Iluz", "65.500.846/0001-36","(84)2344-3443", "iluz@edt.com", 1),
+    (NULL, "PubEdit", "79.985.005/0001-70","(84)6453-4543", "pubEdit@gmail.com", 4),
+    (NULL, "Kabum", "70.067.312/0001-06","(84)5329-3783", "kabum@hyper.com", 2),
+    (NULL, "Porto", "17.508.480/0001-07","(84)0993-3443", "porto@uol.com", 5),
+    (NULL, "Mega", "65.202.121/0001-61","(84)4928-1243", "mega@mega.editor.com", 3)
+;
+
+INSERT INTO book(id, cod_author, cod_publisher, isbn, unity, title, subtitle, status, cod_gender, cod_category)
+  VALUES
+    (NULL, 1, 3, 9788541106993, 1, "Aleatório", NULL, 0, 1, 4),
+    (NULL, 2, 2, 7968525816632, 1, "História é tudo que me deixou", NULL, 0, 2, 1),
+    (NULL, 2, 4, 8373571800587, 1, "Vermelho, Branco e Sangue Azul", NULL, 0, 3, 2),
+    (NULL, 3, 1, 5614827216374, 1, "15 Dias", NULL, 0, 4, 5),
+    (NULL, 4, 5, 2125251257992, 1, "Entre o Céu e o Inferno", NULL, 0, 5, 5)
+;
+
+INSERT INTO functionary(id, name, cpf, telephone, email, salary, cod_address)
+  VALUES
+    (NULL, "Gustavo", 11122233344, "(32)5198-2394", "gustavo@email.com", 1500, 4),
+    (NULL, "Augusto", 43773475645, "(34)1938-4324", "augusto@oul.com", 2000, 3),
+    (NULL, "Xuxa", 73575645645, "(47)7246-4534", "xuxa@outlook.com", 1500, 2),
+    (NULL, "Mário", 52513523552, "(34)4534-5624", "mário@gmail.com", 3500, 1),
+    (NULL, "Sonic", 42534555325, "(54)4366-5452", "sonic@email.com", 4750, 5);
+;
+
+INSERT INTO operation(id, cod_functionary, name, description, day, horary_start, horary_end, workload, cod_library)
+  VALUES
+    (NULL, 1, "Limpeza", "Realiza limpeza dos andares inferiores", 1,"08:00:00", "18:00:00", 50, 3),
+    (NULL, 2, "Atendente", "Atendimento", 2,"08:00:00", "12:00:00", 20, 4),
+    (NULL, 3, "Devolução", "Gestão de devolução", 5,"08:00:00", "13:00:00", 25, 2),
+    (NULL, 4, "Empréstimo", "Gestão de empréstimos", 3,"08:00:00", "18:00:00", 50, 5),
+    (NULL, 5, "Fornecimento", "Recebimento de novos livros", 4,"08:00:00", "17:00:00", 45, 1)
 ;
 
 INSERT INTO session (id, name, cod_category, cod_library)
